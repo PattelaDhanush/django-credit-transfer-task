dff82e7df38b80c43d7f25ab42b5252d8267b902
PattelaDhanush <65483646+PattelaDhanush@users.noreply.github.com>
2020-10-06 00:46:02 +0530

    commit

--- task2/settings.py
+++ task2/settings.py
@@ -125,5 +125,10 @@ DATABASES['default'].update(db_from_env)
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/3.0/howto/static-files/
 
-STATIC_ROOT =  os.path.join(BASE_DIR, 'static')
 STATIC_URL = '/static/'
+if DEBUG:
+        STATICFILES_DIRS = [
+            os.path.join(BASE_DIR, 'static')
+       ]
+    else:
+        STATIC_ROOT = os.path.join(BASE_DIR, 'static')
