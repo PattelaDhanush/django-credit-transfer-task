b0efedd9619d8e8a0a7ae02184d3f02edf5f8c60
PattelaDhanush <65483646+PattelaDhanush@users.noreply.github.com>
2020-10-06 00:52:55 +0530

    commit

--- task2/settings.py
+++ task2/settings.py
@@ -127,8 +127,8 @@ DATABASES['default'].update(db_from_env)
 
 STATIC_URL = '/static/'
 if DEBUG:
-        STATICFILES_DIRS = [
-            os.path.join(BASE_DIR, 'static')
-       ]
-    else:
-        STATIC_ROOT = os.path.join(BASE_DIR, 'static')
+    STATICFILES_DIRS = [
+        os.path.join(BASE_DIR, 'static')
+    ]
+else:
+    STATIC_ROOT = os.path.join(BASE_DIR, 'static')
