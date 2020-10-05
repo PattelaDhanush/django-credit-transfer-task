c9ece7e0b969d1b70d1a86a1d1d7086cda7cc00d
PattelaDhanush <65483646+PattelaDhanush@users.noreply.github.com>
2020-10-06 00:14:07 +0530

    commit

--- task2/settings.py
+++ task2/settings.py
@@ -124,9 +124,9 @@ db_from_env = dj_database_url.config(conn_max_age=600)
 DATABASES['default'].update(db_from_env)
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/3.0/howto/static-files/
+STATICFILES_STORAGE = 'whitenoise.storage.CompressedManifestStaticFilesStorage'
 STATIC_ROOT =  os.path.join(BASE_DIR, 'static')
 STATIC_URL = '/static/'
 STATICFILES_DIRS = [
     os.path.join(BASE_DIR, "static")
 ]
-STATICFILES_STORAGE = 'whitenoise.storage.CompressedManifestStaticFilesStorage'
