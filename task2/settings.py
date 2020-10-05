23acb13945691f086a8f8d50805ede5f2e9b2dff
PattelaDhanush <65483646+PattelaDhanush@users.noreply.github.com>
2020-10-06 01:41:57 +0530

    commit

--- task2/settings.py
+++ task2/settings.py
@@ -76,14 +76,10 @@ WSGI_APPLICATION = 'task2.wsgi.application'
 
 # Database
 # https://docs.djangoproject.com/en/3.0/ref/settings/#databases
-
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.postgresql',
-        'NAME': 'Credit_Info',
-        'USER': 'postgres',
-        'PASSWORD': '123456',
-        'HOST': 'localhost',
+        'ENGINE': 'django.db.backends.sqlite3',
+        'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
     }
 }
 
